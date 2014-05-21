36932c55af3d33c5ff842a687e4b63db4b13e196
MaxLim <i@Maks.Us>
2014-05-20 22:07:16 -0400

    updated script

--- fix.sh
+++ fix.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-AFP_DIR=~/root/       # mouting point 
-#REMOVE_DS=~/.DS_Store # remove DS file in parent directory 
-
 networksetup -setairportpower en0 off
 sleep 1
 for FILE1 in "$@"
@@ -14,7 +11,6 @@ sleep 5
 for FILE1 in "$@"
 do
    ls -la $FILE1
-   #rm $REMOVE_DS
 done
 
 
